24751891a601f90193f15d9af2e881ad5b9b7c05
bchakravorty <b.chakravorty@gmail.com>
2014-09-15 14:32:48 +0530

    Heap, Node and HeapTest BST: Initial version

---
+++ src/com/algorithm/datastructure/BST.java
@@ -0,0 +1,26 @@
+package com.algorithm.datastructure;
+
+/**
+ * Created by biswac on 9/4/2014.
+ */
+public class BST {
+    private Node root;
+
+    public void addNode(Node n){
+
+    }
+
+    public Node removeNode(Node n){
+      return null;
+    }
+
+    public Node floor(int value) {
+        return null;
+    }
+
+
+    public Node ceiling(int value) {
+        return null;
+    }
+
+}
